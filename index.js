82052ca1bdd3f95f73c6e6b594762d87c8a5b24b
damaris <mdamarisnduku@gmail.com>
2022-06-17 17:21:01 +0300

    commit tree 98e7af6b93b23ba8a89cceb6802b24978cbffa9d

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // compare the reversed string to the input
+    return word === reversedWord;
+
 }
 
 /* 
@@ -13,13 +22,18 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("Expecting: ih");
+console.log("=>", reverseString("hi"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("Expecting: tobor");
+console.log("=>", reverseString("robot"));
+
+console.log("");
+
+console.log("Expecting: mom");
+console.log("=>", reverseString("mom"));
 }
 
 module.exports = isPalindrome;
